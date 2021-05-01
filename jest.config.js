e6e9840f518c86c1cefea5ba127010787244293b
vitorregisr <vitorregisrr@gmail.com>
2021-05-01 00:01:20 -0300

    Add custom marker, Add map star background overlay, Improve styling and colors, Fix some small errors

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,9 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/config/jest/assetsTransformer.js',
+  },
 }
