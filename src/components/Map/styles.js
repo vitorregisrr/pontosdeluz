13d89158b2b40c2113aaf45d1fd2f96ea5891d8c
vitorregisr <vitorregisrr@gmail.com>
2021-05-02 14:23:42 -0300

    Minor styling improves

--- src/components/Map/styles.js
+++ src/components/Map/styles.js
@@ -68,7 +68,7 @@ export const MapHeader = styled.header`
 
 export const MapHeaderBrand = styled.img`
   height: 3.8rem;
-  opacity: 0.8;
+  opacity: 0.9;
   transition: opacity 0.2s ease-in;
 
   &:hover {
