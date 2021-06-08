35c78d2182bdd9ea00a81a8c4c293773ca831a14
vitorregisr <vitorregisrr@gmail.com>
2021-06-08 02:20:35 -0300

    Improve SEO tags

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -6,7 +6,7 @@ export default {
     url: 'https://pontosdeluz.com.br',
     site_name: 'Pontos de Luz • Regeneração planetária ao redor do mundo',
     description: 'Regeneração planetária ao redor do mundo.',
-    image: '/img/social-favicon.png',
+    image: '/img/social-favicon.jpg',
   },
   twitter: {
     handle: '@pontosdeluz',
